ada1555141515b4f02e3c5b1c4aa8094a298d770
BLVCKMAsK <masks.studio1010@gmail.com>
2025-04-03 23:37:51 -0700

    update

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import pandas as pd
 import understatapi
 import altair as alt
 from streamlit_option_menu import option_menu
-from demo4 import app as demo4_app
-from demo5 import app as demo5_app
+
+from demo.demo4 import app as demo4_app
+from demo.demo5 import app as demo5_app
 
 selected = option_menu(
     menu_title=None,
